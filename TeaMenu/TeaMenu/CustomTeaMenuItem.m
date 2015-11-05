bb3c7c2451809d9c13cbd1f94a1d07e7df0a38f9
Jan Hindermann <jan@dfragment.net>
2015-11-05 23:34:32 +0100

    Improved UI for tea timer slider, supports scrolling

--- TeaMenu/TeaMenu/CustomTeaMenuItem.m
+++ TeaMenu/TeaMenu/CustomTeaMenuItem.m
@@ -30,9 +30,39 @@
 	return YES;
 }
 
+- (void) scrollWheel:(NSEvent *) theEvent
+{
+	double avgDelta = sqrt( pow(theEvent.deltaX, 2.0f)
+                          + pow(theEvent.deltaY, 2.0f)
+                          + pow(theEvent.deltaZ, 2.0f));
+    
+	if (abs(avgDelta) < 0.25f)
+		return; // too small
+	
+	bool isNegative = !(theEvent.deltaX < 0 // inversion of (negative to the left OR 
+                        || (theEvent.deltaY < 0 // downwards and...
+                            && theEvent.deltaX < 0.1f)); // only small x-movement)
+	
+	int stepDelta = (int)floor(avgDelta * 0.49);
+	if (isNegative)
+		stepDelta *= (-1);
+	
+	[model setMinutes:(model.minutes + stepDelta)]; // using the setter also updates the bindings
+}
+
 @end
 
 // TeaTime data model
 @implementation CustomTeaTimeModel
-@synthesize minutes, teaNotBrewing;
+@synthesize minutes = _minutes;
+@synthesize teaNotBrewing;
+
+const int MAX_MINUTES = 10;
+
+- (void) setMinutes:(NSInteger)minutes {
+    // Custom setter: restrict values to (0, 10]
+    if (minutes > 0 && minutes <= MAX_MINUTES)
+        _minutes = minutes;
+}
+
 @end
